7e1e9c2e4ce91e195e3c7a7c07c11eb027844b49
marinogne <m.n.neves00@gmail.com>
2024-07-22 21:20:35 -0300

    adiciona 003

--- exercicios_python/ex002.py
+++ exercicios_python/ex002.py
@@ -1,2 +1,2 @@
-nome = str(input("Qual é o seu nome? ")).title()
+nome = str(input("Qual é o seu nome?")).title()
 print(f"Seja bem-vindo(a), {nome}!")
---
+++ exercicios_python/ex003.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que leia dois números e 
+mostre a soma entre eles.
+"""
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite mais um número: "))
+soma = num1 + num2
+print(f"O resultado da soma dos dois números é {soma}")
