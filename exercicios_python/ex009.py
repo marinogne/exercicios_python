8b99bac23a005fbc979ef7c41e6fd24356de8eed
marinogne <m.n.neves00@gmail.com>
2024-07-24 14:46:20 -0300

    adiciona o exercicio 009 ao repositorio

---
+++ exercicios_python/ex009.py
@@ -0,0 +1,16 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+num = int(input("Digite um número: "))
+print(f"""
+Tabuada do {num}:
+{num} x 1 = {num}
+{num} x 2 = {num*2}
+{num} x 3 = {num*3}
+{num} x 4 = {num*4}
+{num} x 5 = {num*5}
+{num} x 6 = {num*6}
+{num} x 7 = {num*7}
+{num} x 8 = {num*8}
+{num} x 9 = {num*9}
+{num} x 10 = {num*10}
+""")
